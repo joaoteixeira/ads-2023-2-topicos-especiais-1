647df4dcb69d8ba5c3d9ca3acc2bbbf348dc43f9
joaoteixeira <joao.teixeira@ifro.edu.br>
2023-10-09 17:47:10 -0400

    fix return

--- aula_04-28-08/main.js
+++ aula_04-28-08/main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (request, response) {
-  response.send('API Funcionando...');
+  return response.send('API Funcionando...');
 });
 
 app.listen(3000, function () {
