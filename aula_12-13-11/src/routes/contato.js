3b18b9b2a584aff054361c8ccdaee092f8ff46ca
João Teixeira <joao.teixeira@ifro.edu.br>
2023-11-13 21:58:45 -0400

    aula 13/11

---
+++ aula_12-13-11/src/routes/contato.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const contatoController = require('../controllers/contato.controller');
+
+const router = express.Router();
+
+router.get('/contatos', contatoController.list);
+router.get('/contatos/:codigo', contatoController.show);
+router.post('/contatos', contatoController.create);
+router.put('/contatos/:codigo', contatoController.update);
+router.delete('/contatos/:codigo', contatoController.destroy);
+
+module.exports = router;
